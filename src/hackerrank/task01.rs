9472ce440481f9ff6912427019057c502e94ccd2
ArtemB887 <artem.burdun@e-u.edu.ua>
2025-04-28 10:20:16 +0300

    Update task01.rs

--- src/hackerrank/task01.rs
+++ src/hackerrank/task01.rs
@@ -1,4 +1,27 @@
-#[test]
-fn test1() {
-    assert_eq!(1 + 1, 2);
-}
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'simpleArraySum' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts INTEGER_ARRAY ar as parameter.
+#
+
+def simpleArraySum(ar):
+    return sum(ar)
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    ar_count = int(input().strip())
+    ar = list(map(int, input().rstrip().split()))
+    result = simpleArraySum(ar)
+
+    fptr.write(str(result) + '\n')
+    fptr.close()
